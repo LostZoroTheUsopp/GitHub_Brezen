d988c6c8cec293eb2138f978f80235a44c74914f
Meloun26 <Meloun26@MELOUN26>
2022-04-24 21:54:07 +0200

    Přidejte soubory projektu.

---
+++ GitHub_Brezen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Brezen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float cislo1 = float.Parse(Console.ReadLine());
+            float cislo2 = float.Parse(Console.ReadLine());
+            Console.Clear();
+            if(cislo1 > cislo2)
+            {
+                Console.WriteLine(cislo1);
+                Console.WriteLine(cislo2);
+            }
+            else
+            {
+                Console.WriteLine(cislo2);
+                Console.WriteLine(cislo1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
